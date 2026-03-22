a0164a215dbb1f12f713b5501259bc11a831cc49
Skomarovskyi Kyrylo <skomarovskiykirill@gmail.com>
2026-03-22 16:02:28 +0200

    implement ProtectedRoute

---
+++ src/router/ProtectedRoute.tsx
@@ -0,0 +1,21 @@
+import { type PropsWithChildren } from 'react'
+import { Navigate } from 'react-router'
+import { useAuth } from '../contexts/AuthContext.tsx'
+
+type ProtectedRouteProps = PropsWithChildren<{
+  redirectTo: string
+}>
+
+export function ProtectedRoute({ children, redirectTo }: ProtectedRouteProps) {
+  const { user, loading } = useAuth()
+
+  if (loading) {
+    return null
+  }
+
+  if (!user) {
+    return <Navigate to={redirectTo} replace />
+  }
+
+  return children
+}
