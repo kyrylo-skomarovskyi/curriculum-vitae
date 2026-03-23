5f1de2b752c43089bde581ab6cd813f88c1543b9
Skomarovskyi Kyrylo <skomarovskiykirill@gmail.com>
2026-03-23 16:21:33 +0200

    chore: ignore shadcn ui files

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'src/components/ui']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
