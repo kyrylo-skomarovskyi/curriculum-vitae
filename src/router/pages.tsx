e0bf6e0a92d08b00f05d7781ab9d609073acda22
Skomarovskyi Kyrylo <skomarovskiykirill@gmail.com>
2026-03-23 14:57:20 +0200

    add suspense around lazy pages

--- src/router/pages.tsx
+++ src/router/pages.tsx
@@ -1,8 +1,52 @@
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
+import { ROUTE_PATH } from '@/constants/route-paths.ts';
 
-export const LoginPage = lazy(() => import('./login-page.tsx'));
-export const HomePage = lazy(() => import('./home-page.tsx'));
-export const ProjectsPage = lazy(() => import('./projects-page.tsx'));
-export const ProjectPage = lazy(() => import('./project-page.tsx'));
-export const CreateProjectPage = lazy(() => import('./create-project-page.tsx'));
-export const EditProjectPage = lazy(() => import('./edit-project-page.tsx'));
+const LoginPageLazy = lazy(() => import('./login-page.tsx'));
+const HomePageLazy = lazy(() => import('./home-page.tsx'));
+const ProjectsPageLazy = lazy(() => import('./projects-page.tsx'));
+const ProjectPageLazy = lazy(() => import('./project-page.tsx'));
+const CreateProjectPageLazy = lazy(() => import('./create-project-page.tsx'));
+const EditProjectPageLazy = lazy(() => import('./edit-project-page.tsx'));
+
+export function LoginPage() {
+  return (
+    <Suspense key={ROUTE_PATH.login} fallback={null}>
+      <LoginPageLazy />
+    </Suspense>
+  );
+}
+export function HomePage() {
+  return (
+    <Suspense key={ROUTE_PATH.home} fallback={null}>
+      <HomePageLazy />
+    </Suspense>
+  );
+}
+export function ProjectsPage() {
+  return (
+    <Suspense key={ROUTE_PATH.projects} fallback={null}>
+      <ProjectsPageLazy />
+    </Suspense>
+  );
+}
+export function ProjectPage() {
+  return (
+    <Suspense key={ROUTE_PATH.project} fallback={null}>
+      <ProjectPageLazy />
+    </Suspense>
+  );
+}
+export function CreateProjectPage() {
+  return (
+    <Suspense key={ROUTE_PATH.createProject} fallback={null}>
+      <CreateProjectPageLazy />
+    </Suspense>
+  );
+}
+export function EditProjectPage() {
+  return (
+    <Suspense key={ROUTE_PATH.editProject} fallback={null}>
+      <EditProjectPageLazy />
+    </Suspense>
+  );
+}
