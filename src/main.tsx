38108d7e28b5f901322ea9e7c06a32e0b9ebb5d4
Skomarovskyi Kyrylo <skomarovskiykirill@gmail.com>
2026-03-23 07:22:39 +0200

    add tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { AuthProvider } from './contexts/auth-context/auth-provider.tsx';
 import router from './router/index.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
