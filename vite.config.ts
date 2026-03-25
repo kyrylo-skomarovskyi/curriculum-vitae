fe35497941fdc6d730ec012e9ba95028d2e44917
Skomarovskyi Kyrylo <skomarovskiykirill@gmail.com>
2026-03-25 17:12:36 +0200

    add visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,17 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/curriculum-vitae/',
-  plugins: [tailwindcss(), react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [
+    tailwindcss(),
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    visualizer({ open: true }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
